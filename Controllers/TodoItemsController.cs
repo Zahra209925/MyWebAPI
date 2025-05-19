b4e4a186744b089498364bb6d7864afe350dc63e
Zahra209925 <nabavizahra2099@gmail.com>
2025-05-19 13:07:21 +0300

    Add POST method to TodoItemsController

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -35,5 +35,14 @@ namespace MyWebAPI.Controllers
 
 			return todoItem;
 		}
+
+		[HttpPost]
+		public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+		{
+			_context.TodoItems.Add(todoItem);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+		}
 	}
 }
